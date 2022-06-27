ed68276669dc1cef2946d5f3e1466f920856fc67
Nicolas Gong <gong.qifeng@qq.com>
2022-06-27 21:16:19 +0800

    Update script since python3.10 is not compatible for tensorboard

--- diffusion-point-cloud/initializer.bat
+++ diffusion-point-cloud/initializer.bat
@@ -1,6 +1,6 @@
 @ECHO off
 ECHO "Initial the environment"
-CALL conda create --name ae_gen -y
+CALL conda create --name ae_gen python=3.9 -y
 CALL conda activate ae_gen
 CALL conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
 CALL conda install h5py tqdm tensorboard numpy scipy scikit-learn -y
