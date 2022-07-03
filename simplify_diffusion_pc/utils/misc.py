28a393a149b956e4f7347047febc7e09e89280bb
GONG Qifeng <21042139g@msds.comp.polyu.edu.hk>
2022-07-03 14:31:36 +0800

    add checkpoint manager class

--- simplify_diffusion_pc/utils/misc.py
+++ simplify_diffusion_pc/utils/misc.py
@@ -2,9 +2,11 @@ import os.path
 import time
 import logging
 import logging.handlers
+import torch
 
 THOUSAND = 1000
 MILLION = 1000000
+
 
 def get_new_log_dir(root='./logs', postfix='', prefix=''):
     log_dir = os.path.join(root, prefix + time.strftime('%Y_%m_%d__%H_%M_%S', time.localtime()) + postfix)
@@ -31,4 +33,99 @@ def get_logger(name, log_dir=None):
     return logger
 
 
+class BlackHole(object):
+    def __setattr__(self, name, value):
+        pass
 
+    def __call__(self, *args, **kwargs):
+        return self
+
+    def __getattr__(self, name):
+        return self
+
+
+class CheckpointManager(object):
+
+    def __init__(self, save_dir, logger=BlackHole()):
+        super().__init__()
+        os.makedirs(save_dir, exist_ok=True)
+        self.save_dir = save_dir
+        self.ckpts = []
+        self.logger = logger
+
+        for f in os.listdir(self.save_dir):
+            if f[:4] != 'ckpt':
+                continue
+            _, score, it = f.split('_')
+            it = it.split('.')[0]
+            self.ckpts.append({
+                'score': float(score),
+                'file': f,
+                'iteration': int(it),
+            })
+
+    def get_worst_ckpt_idx(self):
+        idx = -1
+        worst = float('-inf')
+        for i, ckpt in enumerate(self.ckpts):
+            if ckpt['score'] >= worst:
+                idx = i
+                worst = ckpt['score']
+        return idx if idx >= 0 else None
+
+    def get_best_ckpt_idx(self):
+        idx = -1
+        best = float('inf')
+        for i, ckpt in enumerate(self.ckpts):
+            if ckpt['score'] <= best:
+                idx = i
+                best = ckpt['score']
+        return idx if idx >= 0 else None
+
+    def get_latest_ckpt_idx(self):
+        idx = -1
+        latest_it = -1
+        for i, ckpt in enumerate(self.ckpts):
+            if ckpt['iteration'] > latest_it:
+                idx = i
+                latest_it = ckpt['iteration']
+        return idx if idx >= 0 else None
+
+    def save(self, model, args, score, others=None, step=None):
+
+        if step is None:
+            fname = 'ckpt_%.6f_.pt' % float(score)
+        else:
+            fname = 'ckpt_%.6f_%d.pt' % (float(score), int(step))
+        path = os.path.join(self.save_dir, fname)
+
+        torch.save({
+            'args': args,
+            'state_dict': model.state_dict(),
+            'others': others
+        }, path)
+
+        self.ckpts.append({
+            'score': score,
+            'file': fname
+        })
+
+        return True
+
+    def load_best(self):
+        idx = self.get_best_ckpt_idx()
+        if idx is None:
+            raise IOError('No checkpoints found.')
+        ckpt = torch.load(os.path.join(self.save_dir, self.ckpts[idx]['file']))
+        return ckpt
+
+    def load_latest(self):
+        idx = self.get_latest_ckpt_idx()
+        if idx is None:
+            raise IOError('No checkpoints found.')
+        ckpt = torch.load(os.path.join(self.save_dir, self.ckpts[idx]['file']))
+        return ckpt
+
+    def load_selected(self, file):
+        ckpt = torch.load(os.path.join(self.save_dir, file))
+        return ckpt
