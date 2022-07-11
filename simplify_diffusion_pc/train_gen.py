c292bc064759043f00e6d079575b668c3e1b75ea
GONG Qifeng <21042139g@msds.comp.polyu.edu.hk>
2022-07-11 17:29:47 +0800

    Update train_gen.py

--- simplify_diffusion_pc/train_gen.py
+++ simplify_diffusion_pc/train_gen.py
@@ -15,7 +15,7 @@ from evaluation import *
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset_path', type=str, default='./data/shapenet.hdf5')
 parser.add_argument('--scale_mode', type=str, default='shape_unit')
-parser.add_argument('--train_batch_size', type=int, default=64)
+parser.add_argument('--train_batch_size', type=int, default=128)
 parser.add_argument('--val_batch_size', type=int, default=64)
 
 # Model arguments
