6deafd7c19b447f15046b5a8640b0bcbc036040e
Nicolas Gong <gong.nicolas@gmail.com>
2022-07-14 15:58:58 +0800

    remove uselesss repos

--- diffusion-point-cloud/evaluation/__init__.py
+++
@@ -1,1 +0,0 @@
-from .evaluation_metrics import *
--- diffusion-point-cloud/initializer.bat
+++
@@ -1,7 +0,0 @@
-@ECHO off
-ECHO "Initial the environment"
-CALL conda create --name ae_gen python=3.9 -y
-CALL conda activate ae_gen
-CALL conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
-CALL conda install h5py tqdm tensorboard numpy scipy scikit-learn -y
-PAUSE
