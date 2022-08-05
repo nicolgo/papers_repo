9bef3da182c18050571954a0ee043c5ad04bedba
COMP\21042139g <qifeng.gong@connect.polyu.hk>
2022-08-05 15:57:21 +0800

    add a common script to support multi repos

--- common_initializer.bat
+++ common_initializer.bat
@@ -1,10 +1,10 @@
 @ECHO off
 ECHO "Initial the environment"
 CALL conda update conda -y
-CALL conda create --name pointflow -y
-CALL conda activate pointflow
+CALL conda create --name common_env -y
+CALL conda activate common_env
 CALL conda install -c open3d-admin open3d -y
 CALL conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
 CALL conda install -c conda-forge torchdiffeq tensorboardx -y
-CALL conda install matplotlib tensorboard numpy scipy scikit-learn -y
+CALL conda install matplotlib h5py tqdm tensorboard numpy scipy scikit-learn -y
 PAUSE
