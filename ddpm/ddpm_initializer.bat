e411958d62210ba8238613236f1f8e2f39ef3047
COMP\21042139g <qifeng.gong@connect.polyu.hk>
2022-08-05 16:49:55 +0800

    add DDPM environment initializer

---
+++ ddpm/ddpm_initializer.bat
@@ -0,0 +1,11 @@
+@ECHO off
+ECHO "Initial the environment"
+CALL conda update conda -y
+CALL conda create --name ddpm python=3.8 -y
+CALL conda activate ddpm
+CALL conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
+CALL conda install -c conda-forge einops -y
+CALL conda install tqdm pillow -y
+CALL conda install -c fastai accelerate
+CALL pip install ema-pytorch
+PAUSE
