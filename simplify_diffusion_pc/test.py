fd4c7c64cb3d6ef311d62a53ec3d83fa5df0abb3
COMP\21042139g <qifeng.gong@connect.polyu.hk>
2022-08-10 18:37:57 +0800

    visualization latent space with TSNE

--- simplify_diffusion_pc/test.py
+++ simplify_diffusion_pc/test.py
@@ -3,6 +3,8 @@ from torch.utils.data import dataset, DataLoader
 from utils.dataset import *
 from models.vae_gaussian import *
 from models.diffusion import *
+from models.model_factory import *
+from utils.tsne import *
 import time
 
 
@@ -41,6 +43,35 @@ def show_point_cloud():
     show_diffusion_process(show_list)
 
 
+def show_latent_space():
+    train_dataset = ShapeNetData(path='./data/shapenet.hdf5', categories=['airplane'], split='train',
+                                 scale_mode='shape_unit')
+    train_iter = get_data_iterator(DataLoader(train_dataset, batch_size=128, num_workers=0))
+    # load model
+    ckpt = torch.load('./pretrained/flow_1983000.pt')
+    model = (get_model_by_type(ckpt['args'].model_type, ckpt['args']))
+    model.load_state_dict(ckpt['state_dict'])
+    # w = torch.randn([128, ckpt['args'].latent_dim])
+    # z_context = model.flow(w, reverse=True).view(w.size(0), -1)
+    z_total = []
+    for i in range(10):
+        batch = next(train_iter)
+        x = batch['point_cloud']
+        with torch.no_grad():
+            z_mean, z_log_var = model.encoder(x)
+            z_context = reparameterize_gaussian(z_mean, z_log_var).view(x.size(0), -1)
+            if i == 0:
+                z_total = z_context
+            else:
+                z_total = torch.cat([z_total, z_context])
+
+    Y = tsne(z_total.numpy(), 2, 50, 20.0)
+    # pylab.scatter(Y[:, 0], Y[:, 1], 20, labels)
+    pylab.scatter(Y[:, 0], Y[:, 1], 20)
+    pylab.show()
+
+
 if __name__ == "__main__":
     # show_point_cloud()
-    only_leave_latest_file("D:\GEN_2022_07_04__18_50_41")
+    # only_leave_latest_file("D:\GEN_2022_07_04__18_50_41")
+    show_latent_space()
