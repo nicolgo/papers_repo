5568a3def1e29a21b0dc2125963d621a114fe90e
Nicolas Gong <qifeng.gong@connect.polyu.hk>
2022-08-26 20:24:53 +0800

    copy util files to the project

--- my_iddpm/scripts/image_train.py
+++ my_iddpm/scripts/image_train.py
@@ -1,14 +1,16 @@
 import argparse
 
 import torch
-
+from improved_diffusion import logger, dist_util
+from improved_diffusion.image_datasets import load_data
+from improved_diffusion.resample import create_named_schedule_sampler
 from improved_diffusion.script_util import (
     model_and_diffusion_defaults,
     add_dict_to_argparser,
     create_model_and_diffusion,
     args_to_dict,
 )
-from improved_diffusion import logger
+from improved_diffusion.train_util import TrainLoop
 
 
 def create_argparser():
@@ -35,13 +37,44 @@ def create_argparser():
 
 def main():
     args = create_argparser().parse_args()
+
+    dist_util.setup_dist()
     logger.configure()
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logger.log("creating model and diffusion...")
     model, diffusion = create_model_and_diffusion(
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
     model.to(device)
+    schedule_sampler = create_named_schedule_sampler(args.schedule_sampler, diffusion)
+
+    logger.log("create data loader...")
+    data = load_data(
+        data_dir=args.data_dir,
+        batch_size=args.batch_size,
+        image_size=args.image_size,
+        class_cond=args.class_cond,
+    )
+
+    logger.log("training...")
+    TrainLoop(
+        model=model,
+        diffusion=diffusion,
+        data=data,
+        batch_size=args.batch_size,
+        microbatch=args.microbatch,
+        lr=args.lr,
+        ema_rate=args.ema_rate,
+        log_interval=args.log_interval,
+        save_interval=args.save_interval,
+        resume_checkpoint=args.resume_checkpoint,
+        use_fp16=args.use_fp16,
+        fp16_scale_growth=args.fp16_scale_growth,
+        schedule_sampler=schedule_sampler,
+        weight_decay=args.weight_decay,
+        lr_anneal_steps=args.lr_anneal_steps,
+    ).run_loop()
 
 
 if __name__ == "__main__":
