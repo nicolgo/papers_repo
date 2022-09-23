732e757d520c5ab22c81ee1cb706e3f3e25e506e
Nicolas Gong <qifeng.gong@connect.polyu.hk>
2022-09-23 15:23:27 +0800

    custome CIFAR dataset

---
+++ simple_stable_diffusion/ldm/data/CIFAR.py
@@ -0,0 +1,34 @@
+from torch.utils.data import Dataset
+from torchvision import transforms
+from torchvision.datasets import CIFAR10
+
+
+class CustomCIFAR10(Dataset):
+    """Wraps an arbitrary object with __len__ and __getitem__ into a pytorch dataset"""
+
+    def __init__(self):
+        self.data_dir = "dataset/CIFAR10/data"
+        self.dataset = CIFAR10(self.data_dir, train=self.train, download=True)
+        self.data = self.dataset.data
+        self.target = self.dataset.targets
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        example = dict()
+        example["image"] = self.data[idx]
+        example["class_label"] = self.target[idx]
+        return example
+
+
+class CIFAR10Train(CustomCIFAR10):
+    def __init__(self):
+        self.train = True
+        super().__init__()
+
+
+class CIFAR10Validation(CustomCIFAR10):
+    def __init__(self):
+        self.train = False
+        super().__init__()
