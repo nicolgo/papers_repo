8302904fd1616f12a70ef703526479cfa354d212
Nicolas Gong <qifeng.gong@connect.polyu.hk>
2022-10-16 23:36:13 +0800

    add video output

--- simple_video_diffusion/utils/vision_util.py
+++ simple_video_diffusion/utils/vision_util.py
@@ -3,7 +3,7 @@ import torch
 import torchvision
 import PIL
 
-
+# deprecated
 def save_image_grid(img, fname, drange, grid_size, normalize=True):
     if normalize:
         lo, hi = drange
@@ -25,3 +25,30 @@ def save_image_grid(img, fname, drange, grid_size, normalize=True):
         torchvision.io.write_video(f'{fname[:-3]}mp4', torch.from_numpy(img), fps=8)
         imgs = [PIL.Image.fromarray(img[i], 'RGB') for i in range(len(img))]
         imgs[0].save(fname, quality=95, save_all=True, append_images=imgs[1:], duration=100, loop=0)
+
+
+import math
+import numpy as np
+import skvideo
+import skvideo.io
+def save_video_grid(video, fname, nrow=None):
+    b, c, t, h, w = video.shape
+    video = video.permute(0, 2, 3, 4, 1)
+    video = (video.cpu().numpy() * 255).astype('uint8')
+
+    if nrow is None:
+        nrow = math.ceil(math.sqrt(b))
+    ncol = math.ceil(b / nrow)
+    padding = 1
+    video_grid = np.zeros((t, (padding + h) * nrow + padding,
+                           (padding + w) * ncol + padding, c), dtype='uint8')
+    for i in range(b):
+        r = i // ncol
+        c = i % ncol
+
+        start_r = (padding + h) * r
+        start_c = (padding + w) * c
+        video_grid[:, start_r:start_r + h, start_c:start_c + w] = video[i]
+
+    skvideo.io.vwrite(fname, video_grid, inputdict={'-r': '5'})
+    print('saved videos to', fname)
