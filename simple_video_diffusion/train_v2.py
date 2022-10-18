9da870156abf5f2f8706f251bc80fb3fe8c9e220
Nicolas Gong <qifeng.gong@connect.polyu.hk>
2022-10-18 13:18:10 +0800

    Update train_v2.py

--- simple_video_diffusion/train_v2.py
+++ simple_video_diffusion/train_v2.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                                   ).cuda()
 
     trainer = VideoTrainer(diffusion, "D:/ssd/nicol/papers_repo/simple_video_diffusion/data/ucf101",
-                           train_batch_size=1, train_lr=1e-4, save_and_sample_every=1000, train_num_steps=700000,
+                           train_batch_size=2, train_lr=1e-4, save_and_sample_every=1000, train_num_steps=700000,
                            # total training steps
                            gradient_accumulate_every=2,  # gradient accumulation steps
                            ema_decay=0.995,  # exponential moving average decay
