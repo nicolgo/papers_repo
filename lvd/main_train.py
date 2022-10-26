e7d7c0b9c1ac04d9ec66d92a54e65d5e42c40d5f
Nicolas Gong <qifeng.gong@connect.polyu.hk>
2022-10-26 14:31:38 +0800

    add vqvae model

--- lvd/main_train.py
+++ lvd/main_train.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import argparse
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from datasets.video_dataset import VideoData
+from lvd.models.vqvae import VQVAE
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    pl.seed_everything(1234)
+
+    parser = argparse.ArgumentParser()
+    parser = pl.Trainer.add_argparse_args(parser)
+    parser = VQVAE.add_model_specific_args(parser)
+    parser.add_argument('--data_path', type=str, default='/home/wilson/data/datasets/bair.hdf5')
+    parser.add_argument('--sequence_length', type=int, default=16)
+    parser.add_argument('--resolution', type=int, default=64)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--num_workers', type=int, default=8)
+    args = parser.parse_args()
+
+    data = VideoData(args)
+    # pre-make relevant cached files if necessary
+    data.train_dataloader()
+    data.test_dataloader()
+    model = VQVAE(args)
+
+    callbacks = []
+    callbacks.append(ModelCheckpoint(monitor='val/recon_loss', mode='min'))
+
+    kwargs = dict()
+    if args.gpus > 1:
+        kwargs = dict(distributed_backend='ddp', gpus=args.gpus)
+    trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks,
+                                            max_steps=200000, **kwargs)
+
+    trainer.fit(model, data)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
